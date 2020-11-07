969fbde4b06eb804a6af899e8f1e366c9610830d
Zac <zacharyflynn06@gmail.com>
2020-11-07 15:21:26 -0700

    commit tree 04d22c64a738ad383cc8e70041528512cd3e0253

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+class School
+
+    attr_accessor :roster
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def grade(level)
+        @roster[level]
+    end
+
+    def add_student(name, grade)
+        # if @roster[grade]
+        #   @roster[grade] << name
+
+        # else
+        #   @roster[grade] = [name]
+        # end
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def sort
+        # new_hash = {}
+        # @roster.each do |grade, students|
+        #    new_hash[grade] = students.sort
+        # end
+        # new_hash
+        @roster.each do |grade, students|
+            students.sort!
+        end
+    end
+end
